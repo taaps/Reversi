8ad7ce97bba9f4a1ec81cacbb713b1dce800f0ec
Tapasvi <tapasvi101@gmail.com>
2019-09-25 02:40:05 -0400

    Added functionality to check if a given direction is valid as well as how many tiles can be flipped over in a given direction.

--- src/algorithm.c
+++ src/algorithm.c
@@ -2,3 +2,46 @@
 #include <stdbool.h>
 #include "algorithm.h"
 
+// Function returns how many tiles that can be flipped over for a given direction
+int howManyTilesCanBeFlipped(int n, char board[26][26], int numRow, int numCol, char colourOfComputer, int deltaRow, int deltaCol)
+{
+	numRow -= deltaRow;
+	numCol += deltaCol;
+	int count = 0;
+	int totalCountIfDirectionPossible = 0;
+	
+	while ((positionInBounds(n, numRow, numCol)) && (board[numRow][numCol] != colourOfComputer) && (board[numRow][numCol] != 'U'))
+	{
+		count++;
+		numRow -= deltaRow;
+		numCol += deltaCol;
+		
+		if((board[numRow][numCol] == colourOfComputer) && (positionInBounds(n, numRow, numCol)))
+		{
+			totalCountIfDirectionPossible = count;
+			break;
+		}
+	}
+	
+	return totalCountIfDirectionPossible;
+}
+
+// Returns true if direction is valid, returns false if direction is not valid
+bool isDirectionAValidMove(int n, char board[26][26], int numRow, int numCol, char colour, int deltaRow, int deltaCol)
+{
+	numRow -= deltaRow;
+	numCol += deltaCol;
+	bool returnValue = false;
+	
+	while((positionInBounds(n, numRow, numCol)) && (board[numRow][numCol] != colour) && (board[numRow][numCol] != 'U'))
+	{
+		numRow -= deltaRow;
+		numCol += deltaCol;
+		
+		if ((board[numRow][numCol] == 'U') && (positionInBounds(n, numRow, numCol)))
+		{
+			returnValue = true;
+		}
+	}
+	return returnValue;
+}
--- src/algorithm.h
+++ src/algorithm.h
@@ -1,5 +1,10 @@
 #ifndef _ALGORITHM_
 #define _ALGORITHM_
 
+// Function returns how many tiles that can be flipped over for a given direction
+int howManyTilesCanBeFlipped(int n, char board[26][26], int numRow, int numCol, char colourOfComputer, int deltaRow, int deltaCol);
+
+// Returns true if direction is valid, returns false if direction is not valid
+bool isDirectionAValidMove(int n, char board[26][26], int numRow, int numCol, char colour, int deltaRow, int deltaCol);
 
 #endif
