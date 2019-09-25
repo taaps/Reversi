50da5e78c8e250301fbaa0d8e306e9ec24615c7e
Tapasvi <tapasvi101@gmail.com>
2019-09-25 02:46:11 -0400

    Added my own algorithm that determines the best move to place a marker based on a series of factors.

--- src/algorithm.c
+++ src/algorithm.c
@@ -62,3 +62,225 @@ int cornerSideMiddle(int n, int numRow, int numCol)
 	
 	return valueOfPosition;
 }
+
+// Function determines how many tiles can be flipped over
+int changeTilesOnBoardForMyAlgorithm(int n, char board[26][26], int numRow, int numCol, char colourOfPlayer, int deltaRow, int deltaCol)
+{
+	int initialRow = numRow;
+	int initialCol = numCol;
+	numRow -= deltaRow;
+	numCol += deltaCol;
+	bool checkIfDirectionIsValid = false;
+	int countHowManyTilesFlipped = 0;
+	
+	while((board[numRow][numCol] != colourOfPlayer) && (positionInBounds(n, numRow, numCol)) && (board[numRow][numCol] != 'U'))
+	{
+		numRow -= deltaRow;
+		numCol += deltaCol;
+		
+		if((board[numRow][numCol] == colourOfPlayer) && (positionInBounds(n, numRow, numCol)))
+		{
+			checkIfDirectionIsValid = true;
+			break;
+		}
+	}
+	
+	if (checkIfDirectionIsValid == true)
+	{
+		initialRow -= deltaRow;
+		initialCol += deltaCol;
+			
+		do
+		{
+			board[initialRow][initialCol] = colourOfPlayer;
+			countHowManyTilesFlipped++;
+			initialRow -= deltaRow;
+			initialCol += deltaCol;
+			
+		} while (board[initialRow][initialCol] != colourOfPlayer);
+	}
+	
+	return countHowManyTilesFlipped;
+}
+
+// Function determines how many moves my own algorithm can make at a given turn
+double maximizeNumberOfMovesICanMake(int n, char board2[26][26], int row, int col, char colourOfComputer)
+{
+	board2[row][col] = colourOfComputer;
+	int deltaRow = -1;
+	int deltaCol = -1;
+	int howManyTilesFlipped = 0;	//how many tiles flipped when I placed my tile
+	int howManyMovesOpponentHas = 0;
+	
+	int i;
+	int j;
+	for (i=-1; i<=1; i++)
+	{
+		for (j=-1; j<=1; j++)
+		{
+			howManyTilesFlipped += changeTilesOnBoardForMyAlgorithm(n, board2, row, col, colourOfComputer, deltaRow, deltaCol);
+			deltaCol++;
+		}
+		deltaCol = -1;
+		deltaRow++;
+	}
+	
+	//only call if opponent has a possible move for the next turn
+	//howManyMovesOpponentHas = opponentAlgorithm(n, board2, colourOfComputer);	//opponent places an educated move and returns how many possible moves it had
+	
+	if (howManyMovesOpponentHas == 0)
+	{
+		return 1000.0;
+	}
+	
+	//need to find out how many moves you have
+	deltaRow = -1;
+	deltaCol = -1;
+	int numberOfMovesAfterOpponentPlaces = 0;
+	int a;
+	int b;
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			for (a=-1; a<=1; a++)
+			{
+				for (b=-1; b<=1; b++)
+				{
+					if (checkLegalInDirection(n, board2, changeNumberToChar(i), changeNumberToChar(j), colourOfComputer, deltaRow, deltaCol) == true)
+					{
+						numberOfMovesAfterOpponentPlaces++;
+					}
+					deltaCol++;
+				}
+				deltaCol = -1;
+				deltaRow++;
+			}
+			deltaCol = -1;
+			deltaRow = -1;
+		}
+	}
+	
+	
+	/*
+	 * in opponent algorithm, find out how many moves opponent has, if they have 0, return 0 right away
+	 * how many tiles I was able to flip over after I made my move: howManyTilesFlipped
+	 * number of moves opponent has after I made my move: howManyMovesOpponentHas
+	 * number of moves I have after opponent places their move: numberOfMovesAfterOpponentPlaces
+	 * //last thing i need is corner shit
+	 */
+	 
+	 double calculatedValue = 0.0;
+	 
+	calculatedValue = (howManyTilesFlipped*0.1) - (howManyMovesOpponentHas*0.2) + (numberOfMovesAfterOpponentPlaces*0.5) + (cornerSideMiddle(n, row, col)*0.3);
+	
+	return calculatedValue; 
+}
+
+// Return a heuristic computed value for where the best position would be to place a marker
+double finalValue(int n, char board[26][26], int row, int col, char colourOfComputer)
+{
+	char board2[26][26];
+	double calculatedValueToReturn = 0.0;
+	
+	//initialize the second board
+	int i;
+	int j;
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			board2[i][j] = board[i][j];
+		}
+	}
+	
+	calculatedValueToReturn = maximizeNumberOfMovesICanMake(n, board2, row, col, colourOfComputer);
+	
+	return calculatedValueToReturn;	//returns calculated value to computerAlgorithm Function
+}
+
+// Initial version of my own implemented algorithm
+void computerAlgorithm(int n, char board[26][26], char colourOfComputer)
+{
+	char possibleMoves[26][26];
+	double valuesWhereToMakeAMove[26][26];
+	
+	//initialize possibleMoves board
+	int i;
+	int j;
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			possibleMoves[i][j] = 'U';
+			valuesWhereToMakeAMove[i][j] = -10.0;
+		}
+	}
+	
+	int deltaRow = -1;
+	int deltaCol = -1;
+	int a;
+	int b;
+	//finds possible moves and saves them in 'possibleMoves'
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			for (a=-1; a<=1; a++)
+			{
+				for (b=-1; b<=1; b++)
+				{
+					if (checkLegalInDirection(n, board, changeNumberToChar(i), changeNumberToChar(j), colourOfComputer, deltaRow, deltaCol) == true)
+					{
+						possibleMoves[i][j] = colourOfComputer;
+					}
+					deltaCol++;
+				}
+				deltaCol = -1;
+				deltaRow++;
+			}
+			deltaCol = -1;
+			deltaRow = -1;
+		}
+	}
+	
+	double value=0.0;
+	
+	//sends possible moves to a function called finalValue and returns all the calculated values on where opponent should move
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			if (possibleMoves[i][j] == colourOfComputer)
+			{
+				value = finalValue(n, board, i, j, colourOfComputer);
+				valuesWhereToMakeAMove[i][j] = value;
+			}
+		}
+	}
+	
+	//run through valuesWhereToMakeAMove and finds highest value, my alogrithm will place a piece of it's own colour there
+	
+	double maxValue = 0.0;
+	int finalRow;
+	int finalCol;
+	
+	for (i=0; i<n; i++)
+	{
+		for (j=0; j<n; j++)
+		{
+			if (valuesWhereToMakeAMove[i][j] > maxValue)
+			{
+				maxValue = valuesWhereToMakeAMove[i][j];
+				finalRow = i;
+				finalCol = j;
+			}
+		}
+	}
+	
+	board[finalRow][finalCol] = colourOfComputer;
+	findTilePositionAndChange(n, board, colourOfComputer, finalRow, finalCol); 
+	
+	printf("Computer places %c at %c%c.\n", colourOfComputer, changeNumberToChar(finalRow), changeNumberToChar(finalCol));
+
+}
--- src/algorithm.h
+++ src/algorithm.h
@@ -10,4 +10,16 @@ bool isDirectionAValidMove(int n, char board[26][26], int numRow, int numCol, ch
 // Finds value of position at corner, side or in the middle
 int cornerSideMiddle(int n, int numRow, int numCol);
 
+// Function determines how many tiles can be flipped over
+int changeTilesOnBoardForMyAlgorithm(int n, char board[26][26], int numRow, int numCol, char colourOfPlayer, int deltaRow, int deltaCol);
+
+// Function determines how many moves my own algorithm can make at a given turn
+double maximizeNumberOfMovesICanMake(int n, char board2[26][26], int row, int col, char colourOfComputer);
+
+// Return a heuristic computed value for where the best position would be to place a marker
+double finalValue(int n, char board[26][26], int row, int col, char colourOfComputer);
+
+// Initial version of my own implemented algorithm
+void computerAlgorithm(int n, char board[26][26], char colourOfComputer);
+
 #endif
