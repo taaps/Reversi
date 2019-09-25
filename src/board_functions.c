1003d73109b5fc8f375f36801d274abcb87c14be
Tapasvi <tapasvi101@gmail.com>
2019-09-25 02:17:35 -0400

    Added print board functionality that takes a board and outputs its contents.

---
+++ src/board_functions.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function prints the board contents
+void printBoard(int n, char board[26][26])
+{
+	int boardRow = 0;
+	char row[26];
+	char col[26];
+	char initialLetter = 'a';
+	
+	// Sets column and row of outer egdes of the board
+	int i;
+	for (i=0; i<n; i++)
+	{
+		row[i] = initialLetter;
+		col[i] = initialLetter;
+		initialLetter++;
+	}
+	
+	printf("  ");
+	for (i=0; i<n; i++)
+		printf("%c", row[i]);
+	
+	printf("\n");
+	int a;
+	for (i=0; i<n; i++)
+	{
+		printf("%c ", col[i]);
+		
+		for(a=0; a<n; a++)
+			printf("%c", board[boardRow][a]);
+		
+		printf("\n");
+		boardRow++;
+	}
+}
+
---
+++ src/board_functions.h
@@ -0,0 +1,7 @@
+#ifndef _BOARD_FUNCTIONS_
+#define _BOARD_FUNCTIONS_
+
+// Function prints the board contents
+void printBoard(int n, char board[26][26]);
+
+#endif
